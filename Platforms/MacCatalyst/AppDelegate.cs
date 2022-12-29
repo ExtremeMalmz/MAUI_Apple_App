f4e4b9d593bf8f9779e93075baeb5f13f11d838e
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2022-12-29 16:08:39 +0100

    Graph shows
    the graph shows up in button 1 and the buttons are somewhat vertical now

--- Platforms/MacCatalyst/AppDelegate.cs
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,11 @@
 ﻿using Foundation;
+using ID_App;
 
 namespace iphone6moai;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
+	//this dont work solution: Just write mauiprogram till it find it
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
