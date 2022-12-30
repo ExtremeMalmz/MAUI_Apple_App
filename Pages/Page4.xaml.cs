e2ebd995856c5a5c169513e6fb5967b54e87ab03
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2022-12-30 18:22:48 +0100

    Makes it visually appealing
    Graphics upgrade

---
+++ Pages/Page4.xaml.cs
@@ -0,0 +1,9 @@
+namespace ID_App.Pages;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pages/Page5.xaml.cs
@@ -0,0 +1,9 @@
+namespace ID_App.Pages;
+
+public partial class Page5 : ContentPage
+{
+	public Page5()
+	{
+		InitializeComponent();
+	}
+}
